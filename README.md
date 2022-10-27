d17b3f2ef10eafefffd372b7bacfa467ef018ccd
Adriano <adriano@grus.local>
2022-10-27 15:38:07 -0300

    projeto curso

---
+++ README.md
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+# despesas_pessoais
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+=======
+# despesas_pessoais
+>>>>>>> b788a71e63d046a9a027d2d8b8bf663656e63849
---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:despesas_pessoais/components/chart.dart';
+import 'package:despesas_pessoais/components/chart_bar.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+import './components/transaction_form.dart';
+import './components/transaction_list.dart';
+import 'models/transaction.dart';
+ 
+main() => runApp(ExpensesApp());
+ 
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData tema = ThemeData();
+ 
+    return MaterialApp(
+      home: MyHomePage(),
+      theme: tema.copyWith(
+        colorScheme: tema.colorScheme.copyWith(
+          primary: Colors.purple,
+          secondary: Colors.black,
+        ),
+        textTheme: tema.textTheme.copyWith(
+          headline6: const TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
+ 
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+ 
+class _MyHomePageState extends State<MyHomePage> {
+  List<Transaction> transactions = [];
+
+  List<Transaction> get recentTransactions{
+    return transactions.where(
+      (element){
+        return element.date.isAfter(DateTime.now().subtract(
+        const Duration(days: 7)
+      ));
+     }
+    ).toList();
+  }
+ 
+  void addTransaction(String title, double value, DateTime dateTime) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: dateTime
+    );
+ 
+    setState(() {
+      transactions.add(newTransaction);
+    });
+ 
+    Navigator.of(context).pop();
+  }
+ 
+  void openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return TransactionForm(onSubmitted: addTransaction,);
+      },
+    );
+  }
+
+  void removeTransaction(String id){
+    setState(() {
+      transactions.removeWhere((element) => element.id == id);
+    });
+    
+  }
+
+  final appBar = AppBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () => openTransactionFormModal(context),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.3,
+              child: Chart(recentTransaction: recentTransactions)
+            ),
+            Container(
+              height: (MediaQuery.of(context).size.height  - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.7,
+              child: TransactionList(transactions: transactions, removeTransaction: removeTransaction,)
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => openTransactionFormModal(context),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
